1d1792e0fca38e0d6afd6b10df5d5fdcc9450f94
alishahaghrab <sam11011047@gmail.com>
2016-09-03 04:27:04 -0700

    Update taylor-bot.lua

--- bot/taylor-bot.lua
+++ bot/taylor-bot.lua
@@ -233,23 +233,23 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {68747297},--Sudo users
+    sudo_users = {241188407,174410791},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Taylor Team v4
+    about_text = [[aghrab Team v4
     Taylor Team and Taylor Bot Anti spam / anti link
     
     website : 
-    taylortg.ir  ❤️
+    norz.uk  ❤️
     
     admin : 
     
-    @negative_officiall ❤️
+    @king_of_aqrab ❤️
     
     
     channel : 
     
     
-    @taylor_Team ❤️
+    @aqrab_news ❤️
 ]],
     help_text_realm = [[
 Realm Commands:
